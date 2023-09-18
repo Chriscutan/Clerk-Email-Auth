09fd28ff0f7df37c0f6461f6aa3518924456379e
chriscutan nadar <chrisrocks2829@gmail.com>
2023-09-18 18:20:01 +0530

    intial

---
+++ app/(auth)/home.tsx
@@ -0,0 +1,16 @@
+import { Button, StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { useUser } from "@clerk/clerk-expo";
+
+const home = () => {
+  const { user } = useUser();
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Welcome, {user?.emailAddresses[0].emailAddress}</Text>
+    </View>
+  );
+};
+
+export default home;
+
+const styles = StyleSheet.create({});
